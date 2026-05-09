d1a653fa75fd5589c7bf79f3126586807d4a4b9f
noble-osei <boakyenoble9966@gmail.com>
2026-05-09 15:20:05 +0000

    chore: initial commit with express setup

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5004;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
