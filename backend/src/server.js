f20f0a010e21b5086dd7a44456ce1e54d1665395
noble-osei <boakyenoble9966@gmail.com>
2026-05-09 15:36:13 +0000

    set-up database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5004;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
