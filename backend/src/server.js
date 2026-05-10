d4b45cda01d5c03cf4239c90a91ca9a811c99ce7
noble-osei <boakyenoble9966@gmail.com>
2026-05-10 18:56:07 +0000

    set up server with express, dotenv, and middleware; add auth and watchlist routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.js";
+import watchlistRoutes from "./routes/watchlist.js";
+import errorHandler, { notFound } from "./middlewares/errorHandler.js";
 
-dotenv.config();
+dotenv.config({ quiet: true });
 
 const app = express();
 const PORT = process.env.PORT || 5004;
+
+app.use(express.json());
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/watchlist", watchlistRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
