b16501c56c8566801222d49c676fef6ee603f619
noble-osei <boakyenoble9966@gmail.com>
2026-05-11 13:30:10 +0000

    feat: implement routing with Home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
 function App() {
-  
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
