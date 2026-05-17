dd2acabe05ed0883801c5746fb37a3e542aec6bb
noble-osei <boakyenoble9966@gmail.com>
2026-05-17 21:18:58 +0000

    feat: update MovieCard styles and adjust image source for better display

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -2,15 +2,15 @@ import { formatDate } from "../helpers/media.js";
 
 export default function MovieCard({ item }) {
   return (
-    <div className="group flex flex-none flex-col snap-start w-40 shrink-0 transition-all duration-300 hover:scale-105">
+    <div className="group flex flex-none flex-col snap-start w-38 shrink-0 transition-all duration-300 hover:scale-105">
       <img
         className="w-full h-56 object-cover rounded-lg shadow-lg mb-4 transition-all duration-300 group-hover:shadow-2xl"
-        src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w185${item.poster_path}`}
         alt={item.title? item.title: item.name}
         loading="lazy"
       />
 
-      <h4 className="text-sm font-bold">{item.title? item.title: item.name}</h4>
+      <h4 className="text-sm font-bold line-clamp-2">{item.title? item.title: item.name}</h4>
       <p className="text-sm font-light text-base-content/70">
         {formatDate(item.release_date || item.first_air_date)}
       </p>
