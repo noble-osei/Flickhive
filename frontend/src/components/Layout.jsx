23607aa33d4c8149fd7f0adeaded1a32be8593d0
noble-osei <boakyenoble9966@gmail.com>
2026-05-18 12:51:01 +0000

    feat: replace SVG icons with React Icons for Navbar buttons

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar.jsx";
+import { IoSearchOutline } from "react-icons/io5";
+import { FiMenu } from "react-icons/fi";
 
 export default function Layout({ children }) {
   return (
@@ -114,20 +116,7 @@ function Navbar() {
             htmlFor="my-drawer"
             className="btn btn-square btn-ghost lg:hidden"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>{" "}
-            </svg>
+            <FiMenu className="inline-block h-5 w-5" />
           </label>
 
           <Link to="/" className="text-3xl font-bold">
@@ -146,21 +135,7 @@ function Navbar() {
             className="btn btn-ghost btn-circle"
             onClick={() => setShowSearchBar(true)}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />{" "}
-            </svg>
+            <IoSearchOutline className="h-5 w-5" />
           </button>
 
           <Link to="/login" className="btn btn-ghost text-lg rounded-full">
