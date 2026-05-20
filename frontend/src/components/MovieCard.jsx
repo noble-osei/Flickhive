992f7fc01504bf0629f26be4b6f3a999e5669664
noble-osei <boakyenoble9966@gmail.com>
2026-05-20 12:37:26 +0000

    feat: refactor MovieCard and SearchMediaCard to use Link for navigation and improve layout

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 import { formatDate, formatProfession } from "../helpers/media.js";
 
 export default function MovieCard({ item }) {
   return (
-    <div
+    <Link
+      to={`/${item.media_type === "movie" ? "movies" : "tv"}/${item.id}`}
       className="group flex flex-none flex-col snap-start w-38 shrink-0 transition-all 
       duration-300 hover:scale-105"
     >
@@ -20,13 +22,17 @@ export default function MovieCard({ item }) {
       <p className="text-sm font-light text-base-content/70">
         {formatDate(item.release_date || item.first_air_date)}
       </p>
-    </div>
+    </Link>
   );
 }
 
-export function SearchMediaCard({ item }) {
+export function SearchMediaCard({ item, closeSearchBar }) {
   return (
-    <div className="group h-22 flex gap-4 py-2 px-4 hover:bg-secondary/10 border-b border-white/10">
+    <Link
+      to={`/${item.media_type === "movie" ? "movies" : "tv"}/${item.id}`}
+      className="group h-22 flex gap-4 py-2 px-4 hover:bg-secondary/10 border-b border-white/10"
+      onClick={closeSearchBar}
+    >
       <img
         className="w-12 h-full shadow-2xl border border-white/13 object-cover rounded-lg 
           transition-all duration-300 group-hover:shadow-2xl"
@@ -40,14 +46,16 @@ export function SearchMediaCard({ item }) {
         <h4 className="text-sm font-bold line-clamp-1">
           {item.title ? item.title : item.name}
         </h4>
-        <p className="text-xs font-light text-base-content/70 uppercase">
-          {item.media_type === "tv" ? "series" : "Movie"} ·{" "}
-          {item.release_date?.slice(0, 4) || item.first_air_date?.slice(0, 4)}
-        </p>
+        {item.media_type !== "person" && (
+          <p className="text-xs font-light text-base-content/70">
+            {item.media_type === "tv" ? "Series" : "Movie"} ·{" "}
+            {item.release_date?.slice(0, 4) || item.first_air_date?.slice(0, 4)}
+          </p>
+        )}
 
         {item.media_type === "person" && item.known_for.length > 0 && (
-          <p className="text-sm font-light text-base-content/70">{`
-            ${formatProfession(item.known_for_department)}, 
+          <p className="text-xs font-light text-base-content/70">{`
+            ${formatProfession(item.known_for_department)} ·
             ${item.known_for[0].title || item.known_for[0].name}
             (${
               item.known_for[0].release_date?.slice(0, 4) ||
@@ -56,6 +64,6 @@ export function SearchMediaCard({ item }) {
           `}</p>
         )}
       </div>
-    </div>
+    </Link>
   );
 }
